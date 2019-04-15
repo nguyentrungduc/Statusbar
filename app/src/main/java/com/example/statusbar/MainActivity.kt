e5a74af08b4e92c6df0e07f61642847fb60b5594
Admin <nguyenduc049@gmail.com>
2019-04-15 23:04:27 +0700

    add

---
+++ app/src/main/java/com/example/statusbar/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.statusbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
