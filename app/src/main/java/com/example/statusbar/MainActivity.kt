430f4617996398fb90b64c2eb1a6cfbc9bc8b733
Admin <nguyenduc049@gmail.com>
2019-04-16 00:20:34 +0700

    add

--- app/src/main/java/com/example/statusbar/MainActivity.kt
+++ app/src/main/java/com/example/statusbar/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.example.statusbar
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+
+    }
+
+    fun ResolveTransparentStatusBarFlag(): Int {
+        val libs = packageManager.systemSharedLibraryNames
+        var reflect: String? = null
+
+        if (libs == null)
+            return 0
+
+        for (lib in libs) {
+            if (lib == "touchwiz")
+                reflect = "SYSTEM_UI_FLAG_TRANSPARENT_BACKGROUND"
+            else if (lib.startsWith("com.sonyericsson.navigationbar"))
+                reflect = "SYSTEM_UI_FLAG_TRANSPARENT"
+        }
+
+        if (reflect == null)
+            return 0
+
+        try {
+            val field = View::class.java!!.getField(reflect)
+            if (field.getType() === Integer.TYPE)
+                return field.getInt(null)
+        } catch (e: Exception) {
+        }
+
+        return 0
+    }
+
+    fun ApplyTransparentStatusBar() {
+        val window = window
+        if (window != null) {
+            val decor = window.decorView
+            if (decor != null)
+                decor.systemUiVisibility = ResolveTransparentStatusBarFlag()
+        }
     }
 }
